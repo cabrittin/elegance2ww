d5eae098e83749ed680f2fcb9e35e8e68ed3f7c6
Christopher Brittin <cabrittin@gmail.com>
2019-02-14 01:57:32 -0500

    add conditional

--- formatdb.py
+++ formatdb.py
@@ -16,12 +16,14 @@ def image_object_relation(img,obj):
     idImg = dict([(img[i][0],i) for i in range(nimg)])
     _obj,_img = [],[]
     for o in obj:
-        o[3] = idImg[o[3]]
-        _obj.append(o)
+        if o[3] in idImg:
+            o[3] = idImg[o[3]]
+            _obj.append(o)
     for i in img:
-        i[0] = idImg[i[0]]
-        i[1] = i[1].split('.')[0]
-        _img.append(i)
+        if i[0] in idImg:
+            i[0] = idImg[i[0]]
+            i[1] = i[1].split('.')[0]
+            _img.append(i)
     return _img,_obj
 
 if __name__ == '__main__':
